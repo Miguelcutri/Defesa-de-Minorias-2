b6f357cd74d07e0650fdb42d6604545e1af3be1d
Vitor <vitor.cutri@hotmail.com>
2021-01-19 13:52:17 -0300

    Implantando Sobre e implantando rotas

--- DefesaDeMinorias2/src/app/app-routing.module.ts
+++ DefesaDeMinorias2/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { SobreComponent } from './sobre/sobre.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'inicio', pathMatch: 'full'},
+  {path: 'sobre', component: SobreComponent},
+  {path: 'inicio', component: InicioComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
